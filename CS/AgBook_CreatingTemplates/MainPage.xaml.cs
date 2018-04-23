7762b70937e2b129431edb236fd0afc3a204a94e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgBook_CreatingTemplates/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace AgBook_CreatingTemplates {
+    public class Order {
+        public string CompanyName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+
+            List<Order> orderList = new List<Order>();
+
+            orderList.Add(new Order() { CompanyName = "Rancho grande",
+                City = "Buenos Aires", Country = "Argentina",
+                Quantity = 92, UnitPrice = 576.55 });
+            orderList.Add(new Order() { CompanyName = "Hanari Carnes",
+                City = "Rio de Janeiro", Country = "Brazil",
+                Quantity = 839, UnitPrice = 1396.10 });
+            orderList.Add(new Order() { CompanyName = "Blondel père et fils",
+                City = "Strasbourg", Country = "France",
+                Quantity = 700, UnitPrice = 848.75 });
+            orderList.Add(new Order() { CompanyName = "Blauer See Delikatessen",
+                City = "Mannheim", Country = "Germany",
+                Quantity = 140, UnitPrice = 347.05 });
+            orderList.Add(new Order() { CompanyName = "Antonio Moreno Taquería",
+                City = "México D.F.", Country = "Mexico",
+                Quantity = 359, UnitPrice = 369.23 });
+
+            book.DataSource = orderList;
+        }
+    }
+}
---
+++ VB/AgBook_CreatingTemplates/MainPage.xaml.vb
@@ -0,0 +1,69 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections.Generic
+Imports System.Windows.Controls
+
+Namespace AgBook_CreatingTemplates
+	Public Class Order
+		Private privateCompanyName As String
+		Public Property CompanyName() As String
+			Get
+				Return privateCompanyName
+			End Get
+			Set(ByVal value As String)
+				privateCompanyName = value
+			End Set
+		End Property
+		Private privateCountry As String
+		Public Property Country() As String
+			Get
+				Return privateCountry
+			End Get
+			Set(ByVal value As String)
+				privateCountry = value
+			End Set
+		End Property
+		Private privateCity As String
+		Public Property City() As String
+			Get
+				Return privateCity
+			End Get
+			Set(ByVal value As String)
+				privateCity = value
+			End Set
+		End Property
+		Private privateUnitPrice As Double
+		Public Property UnitPrice() As Double
+			Get
+				Return privateUnitPrice
+			End Get
+			Set(ByVal value As Double)
+				privateUnitPrice = value
+			End Set
+		End Property
+		Private privateQuantity As Integer
+		Public Property Quantity() As Integer
+			Get
+				Return privateQuantity
+			End Get
+			Set(ByVal value As Integer)
+				privateQuantity = value
+			End Set
+		End Property
+	End Class
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+
+			Dim orderList As New List(Of Order)()
+
+			orderList.Add(New Order() With {.CompanyName = "Rancho grande", .City = "Buenos Aires", .Country = "Argentina", .Quantity = 92, .UnitPrice = 576.55})
+			orderList.Add(New Order() With {.CompanyName = "Hanari Carnes", .City = "Rio de Janeiro", .Country = "Brazil", .Quantity = 839, .UnitPrice = 1396.10})
+			orderList.Add(New Order() With {.CompanyName = "Blondel père et fils", .City = "Strasbourg", .Country = "France", .Quantity = 700, .UnitPrice = 848.75})
+			orderList.Add(New Order() With {.CompanyName = "Blauer See Delikatessen", .City = "Mannheim", .Country = "Germany", .Quantity = 140, .UnitPrice = 347.05})
+			orderList.Add(New Order() With {.CompanyName = "Antonio Moreno Taquería", .City = "México D.F.", .Country = "Mexico", .Quantity = 359, .UnitPrice = 369.23})
+
+			book.DataSource = orderList
+		End Sub
+	End Class
+End Namespace
